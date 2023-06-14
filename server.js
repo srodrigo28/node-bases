4f7c8d5614fdbb4449483e41a87fffce9a15519e
Sebastião Rodrigo <rodrigoexer1@gmail.com>
2023-06-14 15:24:45 -0300

    updated

--- server.js
+++ server.js
@@ -5,31 +5,6 @@
 const express = require('express')
 const app = express()
 
-app.get('/', (req, res) => {
-    res.send('<h1>Hello, world!</h1>')
-})
-
-app.get('/contato', (req, res) => {
-    res.send(`
-        <h1>Contato</h1>
-        <form method="post" action="/form">
-            Nome: <input type="text" name="nome" />
-            Contato: <input type="text" name="contato" />
-            <button>Enviar</button>
-        </form>
-    `)
-})
-
-app.post('/form', (req, res) => {
-    res.send(`
-        <h1>Cadastrado com sucesso</h1>
-
-        <a href="/contato">
-            <button>Voltar</button>
-        </a>
-    `)
-})
-
-app.listen(3000, () => {
-    console.log('Server on port 3000!')
+app.listen(3001, () => {
+    console.log('Server On Port 3000!')
 })
